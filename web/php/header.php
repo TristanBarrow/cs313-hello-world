028c6e9a4b2b8cdbfa00fd99cf3227c9473db27c
Tristan Barrow <tristanmarkbarrow@gmail.com>
2018-05-03 16:23:29 -0600

    fixed header href attributes

--- web/php/header.php
+++ web/php/header.php
@@ -1,5 +1,5 @@
 <div class="nav-bar">
-  <a href='./about-us.php' class="nav-bar__about-us">About Us</a>
-  <a href='./../home.php' class="nav-bar__home">Home</a>
-  <a href='./login.php' class="nav-bar__login">Login</a>
+  <a href='https://cs313-tristan-barrow-homepage.herokuapp.com/php/about-us.php' class="nav-bar__about-us">About Us</a>
+  <a href='https://cs313-tristan-barrow-homepage.herokuapp.com/home.php' class="nav-bar__home">Home</a>
+  <a href='https://cs313-tristan-barrow-homepage.herokuapp.com/php/login.php' class="nav-bar__login">Login</a>
 </div>
