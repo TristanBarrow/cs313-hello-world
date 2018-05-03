be655bdb46375d58022e97f11a39a9b76f3796ed
Tristan Barrow <tristanmarkbarrow@gmail.com>
2018-05-03 15:49:20 -0600

    added two home pages

--- web/home.php
+++ web/home.php
@@ -4,13 +4,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Hello World</title>
+  <title>Document</title>
 </head>
 <body>
-  <?php 
-    for ($x = 0; $x < 10; $x++) {
-      echo "<div>mydiv$x</div>";
-    }
-  ?>
+  my page 1
 </body>
 </html>
