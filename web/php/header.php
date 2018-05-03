81926d86927c402aa0721a71db89e6555aee69de
Tristan Barrow <tristanmarkbarrow@gmail.com>
2018-05-03 16:45:40 -0600

    finished standard stuff for group project

--- web/php/header.php
+++ web/php/header.php
@@ -1,5 +1,20 @@
 <div class="nav-bar">
-  <a href='https://cs313-tristan-barrow-homepage.herokuapp.com/php/about-us.php' class="nav-bar__about-us">About Us</a>
-  <a href='https://cs313-tristan-barrow-homepage.herokuapp.com/home.php' class="nav-bar__home">Home</a>
-  <a href='https://cs313-tristan-barrow-homepage.herokuapp.com/php/login.php' class="nav-bar__login">Login</a>
+  <a 
+    href='https://cs313-tristan-barrow-homepage.herokuapp.com/php/about-us.php' 
+    class="nav-bar__about-us <?php if($activePage === "AboutUs") { echo "active"; }?>"
+  >
+    About Us
+  </a>
+  <a 
+    href='https://cs313-tristan-barrow-homepage.herokuapp.com/home.php' 
+    class="nav-bar__home <?php if($activePage === "Home") { echo "active"; }?>"
+  >
+    Home
+  </a>
+  <a 
+    href='https://cs313-tristan-barrow-homepage.herokuapp.com/php/login.php' 
+    class="nav-bar__login <?php if($activePage === "Login") { echo "active"; }?>"
+  >
+    Login
+  </a>
 </div>
--- web/php/login.php
+++ web/php/login.php
@@ -4,11 +4,13 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <link rel="stylesheet" href="../css/active-page.css">
+  <title>Login</title>
 </head>
 <body>
-<?php 
-    include './php/header.php';
+  <?php 
+    $activePage = "Login";
+    include './header.php';
   ?>
   <h1>This is my home</h1>
 </body>
