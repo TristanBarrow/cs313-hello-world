72c3ae073ff7612c706f3b2177c841f3efed5dd7
Tristan Barrow <tristanmarkbarrow@gmail.com>
2018-05-04 20:00:31 -0600

    fixed extra imported code

--- web/php/links.php
+++ web/php/links.php
@@ -1,21 +1,13 @@
-<div class='body'>
-    <h1 class='body__title'>My Assignments</h1>
-    <div class="body__links">
-        <div class='links'>
-          <?php 
-            $links = array(
-              './php/hello-world.php' => 'Hello World',
-            );
+<?php 
+  $links = array(
+    './php/hello-world.php' => 'Hello World',
+  );
 
-            foreach ($links AS $href => $text) {
-              echo "<a class='links__link' href='";
-              echo $href;
-              echo "'>";
-              echo $text;
-              echo "</a>";
-            }
-          ?>
-        </div>
-    </div>
-
-  </div>
+  foreach ($links AS $href => $text) {
+    echo "<a class='links__link' href='";
+    echo $href;
+    echo "'>";
+    echo $text;
+    echo "</a>";
+  }
+?>
