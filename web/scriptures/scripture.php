b7b9c2e068f93976f06ac9440de9f09f5b414d2f
Tristan Barrow <tristanmarkbarrow@gmail.com>
2018-05-29 17:01:24 -0600

    removed comment

--- web/scriptures/scripture.php
+++ web/scriptures/scripture.php
@@ -1,6 +1,8 @@
 <p><b><?php echo $book . " " . $chapter . ":" . $verse;?></b><?php echo " - \"" . $content . "\""; ?></p>
-<!-- <ul>
+<ul>
   <?php 
-    var_dump($topics);
+    foreach ($topics as $topic) {
+      echo "<li>" . $topic['topic'] . "</li>";
+    } 
   ?>
-</ul> -->
+</ul>
