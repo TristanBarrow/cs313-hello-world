a2158a2408d3f236e69a039654757e6f2e369d65
Tristan Barrow <tristanmarkbarrow@gmail.com>
2018-05-29 16:59:36 -0600

    var_dump

--- web/scriptures/scripture.php
+++ web/scriptures/scripture.php
@@ -1,8 +1,6 @@
 <p><b><?php echo $book . " " . $chapter . ":" . $verse;?></b><?php echo " - \"" . $content . "\""; ?></p>
 <!-- <ul>
   <?php 
-    foreach ($topics as $topic) {
-      echo "<li>" . $topic['topic'] . "</li>";
-    } 
+    var_dump($topics);
   ?>
 </ul> -->
