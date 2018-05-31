49a766259fff4c7e3551036c1b0ee3c691979c0e
Tristan Barrow <tristanmarkbarrow@gmail.com>
2018-05-31 16:27:35 -0600

    added course

--- web/notes/course.php
+++ web/notes/course.php
@@ -1,5 +1,18 @@
 <?php
   $courseId = $_GET["course_id"];
+  require_once("./initDB.php");
+  $db = get_db();
+  if (!isset($db)) {
+    die("DB Connection was not set");
+  }
+
+  $query = "SELECT id, name, number FROM course WHERE id=:id";
+  $stmt = $db->prepare($query);
+    // Bind values
+  $stmt->bindValue(":id", $courseId, PDO::PARAM_INT);
+  $stmt->execute();
+  $course = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 ?>
 
 <!DOCTYPE html>
@@ -8,11 +21,11 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <title>NOTES FOR</title>
 </head>
 <body>
   <?php
-    echo $courseId;
+    var_dump($course);
   ?>
 </body>
 </html>
