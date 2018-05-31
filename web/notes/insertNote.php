9bab87a5a6ddf3386d3c03c55ad028699f4555ae
Tristan Barrow <tristanmarkbarrow@gmail.com>
2018-05-31 16:48:56 -0600

    inserting

--- web/notes/insertNote.php
+++ web/notes/insertNote.php
@@ -3,7 +3,19 @@
   $date = htmlspecialchars($_POST["date"]);
   $content = htmlspecialchars($_POST["content"]);
 
-  echo $id;
-  echo $date;
-  echo $content;
+  // echo $id;
+  // echo $date;
+  // echo $content;
+
+  $query = "INSERT INTO note (course_id, content, date) VALUES (:id, :content, :date)";
+
+  $stmt = $db->prepare($query);
+  $stmt->bindValue(":id", $id, PDO::PARAM_INT);
+  $stmt->bindValue(":date", $date, PDO::PARAM_INT);
+  $stmt->bindValue(":content", $content, PDO::PARAM_INT);
+  // Bind values
+  $stmt->execute();
+
+  echo 'inserted';
+
 ?>
