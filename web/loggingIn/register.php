4ea72ede2eaa027a06610570df115b31e2c2875c
Tristan Barrow <tristanmarkbarrow@gmail.com>
2018-06-05 16:28:40 -0600

    added register

---
+++ web/loggingIn/register.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <form action="./create_user.php" method="post">
+    <p>User: </p>
+    <input type="text" name="username" id="username">
+    <p>Passowrd: </p>
+    <input type="password" name="password" id="password">
+    <button type="submit">Sign Up</button>
+  </form>
+</body>
+</html>
